64d3c2ac6a38c749aec78afc88dd6ea7caeea84b
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2023-12-15 19:55:38 +0000

    Add pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,18 +88,50 @@ const upperCasedCharacters = [
   'Z'
 ];
 
+// * Generate a password when the button is clicked
+//   * Present a series of prompts for password criteria
+//     * Length of password
+//       * At least 8 characters but no more than 128.
+//     * Character types
+//       * Lowercase
+//       * Uppercase
+//       * Numeric
+//       * Special characters ($@%&*, etc)
+//   * Code should validate for each input and at least one character type should be selected
+//   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+
+// Globals:
+// Password length
+// Character sets [special, numeric, lower, upper])
+
 // Function to prompt user for password options
 function getPasswordOptions() {
 
+  // Prompt and save password options:
+    // Choose number between 8 and 128 characters and save to global variable
+    // Include lowercase          Y/N
+    // Include uppercase          Y/N
+    // Include numeric            Y/N
+    // Include Special characters Y/N
+      // Check at least one character set has been selected 
+  
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  
+  // Not sure we need this function
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+
+    // Get password options
+    // Join the chosen character sets into a single array
+    // Rearrange the order of password array elements
+    // Trim the array length to the chosen password size
+    // Return generated password as a string
 
 }
 
