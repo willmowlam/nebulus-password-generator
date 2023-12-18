3a4bcdc3f3c72018c056b414b2bd6d3095fea128
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2023-12-18 17:01:58 +0000

    Consistent code indentation and spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,82 +106,81 @@ const maxPassLength = 128;
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-    // Prompt and save password options:
-    
-    // Choose number between min and max characters and save to global variable
-    let askLength = prompt(`How long do you want your password to be? \nEnter a number from ${minPassLength} to ${maxPassLength}.`);
-    
-    // Did we get a value
-    if (askLength === null) {
-      // Cancel button
+  // Prompt and save password options:
+  
+  // Choose number between min and max characters and save to global variable
+  let askLength = prompt(`How long do you want your password to be? \nEnter a number from ${minPassLength} to ${maxPassLength}.`);
+  
+  // Did we get a value
+  if (askLength === null) {
+    // Cancel button
+    return false;
+  } else {
+    // Is it valid
+    if ((isNaN(askLength)) || (askLength < minPassLength) || (askLength > maxPassLength)) {
+      alert(`Please enter a number from ${minPassLength} to ${maxPassLength}.`);
       return false;
     } else {
-      // Is it valid
-      if ((isNaN(askLength)) || (askLength < minPassLength) || (askLength > maxPassLength)) {
-        alert(`Please enter a number from ${minPassLength} to ${maxPassLength}.`);
-        return false;
-      } else {
-        // Save length to global variable
-        passOpts.passLength = parseInt(askLength);
-      }
+      // Save length to global variable
+      passOpts.passLength = parseInt(askLength);
     }
+  }
 
-    console.log(`Password length: ${passOpts.passLength}`);
+  console.log(`Password length: ${passOpts.passLength}`);
 
-    // Include lowercase          Y/N
-    let askLowercase = confirm("Would you like to include lowercase characters? \nSelect OK for Yes or Cancel for No.");
+  // Include lowercase          Y/N
+  let askLowercase = confirm("Would you like to include lowercase characters? \nSelect OK for Yes or Cancel for No.");
 
-    if (askLowercase) {
-      passOpts.passCharset.useLower = true;
+  if (askLowercase) {
+    passOpts.passCharset.useLower = true;
+  } else {
+    passOpts.passCharset.useLower = false;
+  }
+
+  console.log(`Use Lowercase: ${passOpts.passCharset.useLower}`);
+
+  // Include uppercase          Y/N
+  let askUppercase = confirm("Would you like to include uppercase characters? \nSelect OK for Yes or Cancel for No.");
+
+  if (askUppercase) {
+    passOpts.passCharset.useUpper = true;
+  } else {
+    passOpts.passCharset.useUpper = false;
+  }
+
+  console.log(`Use Uppercase: ${passOpts.passCharset.useUpper}`);
+
+  // Include numeric            Y/N
+  let askNumeric = confirm("Would you like to include numeric characters? \nSelect OK for Yes or Cancel for No.");
+
+  if (askNumeric) {
+    passOpts.passCharset.useNumeric = true;
+  } else {
+    passOpts.passCharset.useNumeric = false;
+  }
+
+  console.log(`Use Numeric: ${passOpts.passCharset.useNumeric}`);
+
+  // Include Special characters Y/N
+  let askSpecial = confirm("Would you like to include special characters (eg @, %, +)? \nSelect OK for Yes or Cancel for No.");
+
+  if (askSpecial) {
+    passOpts.passCharset.useSpecial = true;
+  } else {
+    passOpts.passCharset.useSpecial = false;
+  }
+
+  console.log(`Use Special: ${passOpts.passCharset.useSpecial}`);
+
+  // Check at least one character set has been selected
+  if (!askSpecial && !askLowercase && !askUppercase && !askNumeric) {
+    if (confirm("You haven't selected any charactersets. \nWould you like to select your options again?")) {
+      // Redo selection of password options
+      getPasswordOptions();
     } else {
-      passOpts.passCharset.useLower = false;
+      return false;
     }
-
-    console.log(`Use Lowercase: ${passOpts.passCharset.useLower}`);
-
-    // Include uppercase          Y/N
-    let askUppercase = confirm("Would you like to include uppercase characters? \nSelect OK for Yes or Cancel for No.");
-
-    if (askUppercase) {
-      passOpts.passCharset.useUpper= true;
-    } else {
-      passOpts.passCharset.useUpper = false;
-    }
-
-    console.log(`Use Uppercase: ${passOpts.passCharset.useUpper}`);
-
-    // Include numeric            Y/N
-    let askNumeric = confirm("Would you like to include numeric characters? \nSelect OK for Yes or Cancel for No.");
-
-    if (askNumeric) {
-      passOpts.passCharset.useNumeric= true;
-    } else {
-      passOpts.passCharset.useNumeric = false;
-    }
-
-    console.log(`Use Numeric: ${passOpts.passCharset.useNumeric}`);
-
-    // Include Special characters Y/N
-    let askSpecial = confirm("Would you like to include special characters (eg @, %, +)? \nSelect OK for Yes or Cancel for No.");
-
-    if (askSpecial) {
-      passOpts.passCharset.useSpecial= true;
-    } else {
-      passOpts.passCharset.useSpecial = false;
-    }
-
-    console.log(`Use Special: ${passOpts.passCharset.useSpecial}`);
-
-    // Check at least one character set has been selected
-    if (!askSpecial && !askLowercase && !askUppercase && !askNumeric) {
-      if (confirm("You haven't selected any charactersets. \nWould you like to select your options again?")) {
-        // Redo selection of password options
-        getPasswordOptions();
-      } else {
-        return false;
-      }
-
-    }
+  }  
 }
 
 // Function for getting a random element from an array
@@ -207,26 +206,26 @@ function generatePassword() {
   let passwordString = "";
 
   // Get password options
-  if (false === getPasswordOptions()){
+  if (false === getPasswordOptions()) {
     return "";
   };
 
   // Populate the Password Character Pool with chosen charactersets
-  if (passOpts.passCharset.useSpecial){
+  if (passOpts.passCharset.useSpecial) {
     passwordCharPool.push(...specialCharacters);
   }
-  if (passOpts.passCharset.useNumeric){
+  if (passOpts.passCharset.useNumeric) {
     passwordCharPool.push(...numericCharacters);
   }
-  if (passOpts.passCharset.useLower){
+  if (passOpts.passCharset.useLower) {
     passwordCharPool.push(...lowerCasedCharacters);
   }
-  if (passOpts.passCharset.useUpper){
+  if (passOpts.passCharset.useUpper) {
     passwordCharPool.push(...upperCasedCharacters);
   }
 
   // The pool should never be zero here
-  if (passwordCharPool.length === 0){
+  if (passwordCharPool.length === 0) {
     alert("There has been an error.");
     return "";
   }
